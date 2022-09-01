be4bfb912c328a0973d2ccaec6e79a68b8040699
livingchaoss <101403441+livingchaoss@users.noreply.github.com>
2022-09-01 11:46:14 -0700

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -35,4 +35,26 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
-            
+
+    def __str__(self):
+        """Returns a human-readable string representaion
+        of an instance."""
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+        
+    def save(self):
+        """Updates the updated_at attribute
+        with the current datetime."""
+
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Returns a dicitionary representation of an instance."""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
